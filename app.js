896bc2a4ecccb3d955ad41e942ceddf6773223de
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-05-06 11:29:21 +0300

    таблица с счётчиком

--- app.js
+++ app.js
@@ -17,12 +17,17 @@ const server = http.createServer((req, res) => {
 			console.log(`Получен запрос на /stats`);
 			res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 			count.user_agent++;
-			res.end(`<table>
+			res.end(`<table border= "1">
 			<tr><td>user-agent:</td>
 			<td>request:</td></tr>
 			<tr><td>${req.headers['user-agent']}</td><td>${count.user_agent}</td></tr>
 			</table>`);	
 		}
+        // if(req.url == '/' && (req.url != "/stats" || req.url != "/serve")){
+        //     console.log('Получен запрос на ytbpdtcnysq путь')
+        //     res.writeHead(400, {'Content-Type': 'text/plain; charset=utf-8'});
+        //     res.end('400 Bad Request');
+        // }
     }
 
 })
