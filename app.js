97b2f92846647c3462347f9cb8b1d7a0a02c79e4
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-05-06 10:49:22 +0300

    /stats запрос

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const HOST = 'localhost';
 const PORT = 5500;
+let count = {user_agent: 0};
 
 const server = http.createServer((req, res) => {
     if(req.method == 'GET'){
@@ -12,7 +13,18 @@ const server = http.createServer((req, res) => {
             res.end('Hello');
 
         }
+        if(req.url === "/stats"){
+			console.log(`Получен запрос на /stats`);
+			res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+			count.user_agent++;
+			res.end(`<table>
+			<tr><td>user-agent:</td>
+			<td>request:</td></tr>
+			<tr><td>${req.headers['user-agent']}</td><td>${count.user_agent}</td></tr>
+			</table>`);	
+		}
     }
+
 })
 
 server.listen(PORT, HOST, () => {
