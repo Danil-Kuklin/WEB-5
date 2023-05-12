11872099f87c33695238eb869118d5dbacc23d04
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-05-12 21:47:03 +0300

    post  запросы

--- app.js
+++ app.js
@@ -35,16 +35,32 @@ const server = http.createServer((req, res) => {
         }
     }
 
-    if(req.method == "POST"){
+    else if(req.method == "POST"){
 		if(req.url === "/comments"){
-			console.log(`Получен запрос на /comments`);
+                console.log(`Получен запрос на /comments`);
+                res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                let users = '';
+                req.on("data", (chunk) => {
+                    users += chunk.toString();
+                });
+
+                req.on("end", () => {
+                    let param = JSON.parse(users);
+                    com += JSON.stringify(param);
+                    console.log(param);
+                    res.end("Данные успешно отправлены!");
+                });
+            }
             
-		}
+        else {
+            res.writeHead(400, { "Content-Type": "text/plain; charset=utf-8" });
+            res.end("400 Bad Request!");
+        }
 	}
-})
+});
 
 
 
 server.listen(PORT, HOST, () => {
     console.log(`Начало работы сервера http://${HOST}:${PORT}`)
-})
+});
