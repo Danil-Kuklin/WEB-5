bec8c8307f1a4fd2fc438f1871d382809916271d
Kuklin Danil <Danil-Cool-200@mail.ru>
2023-06-02 22:29:52 +0300

    7

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const router = require('./router/router');
 const express = require('express');
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const HOST = "127.0.0.1";
 const PORT = 5500;
@@ -8,17 +10,18 @@ const app = express();
 
 app.use(express.static('public'));
 app.use("/v1", router);
+app.use(morgan('tiny'));
 
 app.listen(PORT, HOST, () => {
   console.log(`Начало работы сервера http://${HOST}:${PORT}`);
 });
 
 
-app.use(function(err, req, res, next) {
-	if(err.statusCode)
-	{
-		res.status(err.statusCode).json(err.message);
-	}else{
-		res.status(400).json("400 Bad request");
-	}
-});
+// app.use(function(err, req, res, next) {
+// 	if(err.statusCode)
+// 	{
+// 		res.status(err.statusCode).json(err.message);
+// 	}else{
+// 		res.status(400).json("400 Bad request");
+// 	}
+// });
